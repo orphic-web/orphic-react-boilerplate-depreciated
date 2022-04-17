633227a7a5dc2964aaec4500a59cf21c02159a15
OliCote <olivier@orphic.ca>
2022-04-17 12:21:30 -0400

    #3 setup CF, ionic, CD, etc..

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-duplicates */
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/functions';
+import 'firebase/storage';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+const functions = firebase.functions();
+
+if (process.env.REACT_APP_LOCALHOST_STATE) {
+  firebase.functions().useEmulator('localhost', 5001);
+  db.useEmulator('localhost', 8080);
+}
+
+export {
+  functions, db,
+};
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,26 +1,44 @@
 import {
   IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
 } from '@ionic/react';
-
+import { useEffect } from 'react';
+import { db } from '../FirebaseConfig';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
-const Tab1: React.FC = () => (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>Tab 1</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent fullscreen>
-      <IonHeader collapse="condense">
+const Tab1: React.FC = () => {
+  useEffect(() => {
+    try {
+      console.log('asdasd');
+      db.collection('Families').add({
+        test: 'asdasd',
+      }).then(() => {
+        console.log('asdads');
+      }).catch((e: any) => {
+        console.log(e);
+      });
+    } catch (e: any) {
+      console.error(e);
+    }
+  }, []);
+
+  return (
+    <IonPage>
+      <IonHeader>
         <IonToolbar>
-          <IonTitle size="large">Tab 1</IonTitle>
+          <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <ExploreContainer name="Tab 1 page" />
-    </IonContent>
-  </IonPage>
-);
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 1</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 1 page" />
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default Tab1;
