8d029e26e3c663dc11e9af45e19d311c5665ada6
OliCote <olivier@orphic.ca>
2022-10-08 11:34:53 -0400

    updated type documentation

--- src/models/CustomAlert.ts
+++ src/models/CustomAlert.ts
@@ -1,8 +1,15 @@
 type CustomAlertType = {
-    id: string,
-    message: string,
-    severity: 'error' | 'success' | 'info',
-    dismiss: boolean,
+   /** Alert unique id */
+   id: string,
+
+   /** Alert message */
+   message: string,
+
+   /** Alert type */
+   severity: 'error' | 'success' | 'info',
+
+   /** Alert display state */
+   dismiss: boolean,
 }
 
 export default CustomAlertType;
--- src/models/MenuItem.ts
+++ src/models/MenuItem.ts
@@ -1,6 +1,11 @@
 type MenuItem = {
+    /** Menu item display title */
     title: string,
+
+    /** Menu item redirection href url */
     href: string,
+
+    /** Menu item display icon */
     icon: any
 }
 
